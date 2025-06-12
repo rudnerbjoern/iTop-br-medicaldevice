1f2db535a33abd9d35fdbdefa8c9f077070182d0
Bjoern Rudner <rudnerbjoern@hotmail.com>
2025-06-12 11:57:34 +0200

    set compatibility to iTop 3.2

--- br-medicaldevice/dictionaries/fr.dict.br-medicaldevice.php
+++ br-medicaldevice/dictionaries/fr.dict.br-medicaldevice.php
@@ -3,7 +3,7 @@
 /**
  * @copyright   Copyright (C) 2024-2025 Björn Rudner
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
- * @version     2025-06-03
+ * @version     2025-06-12
  *
  * Localized data
  */
